361985df9870853d6d7dc63dbdbc7acb08d7bcb9
AbhishekSuraj0 <ab8948464510gmmail.com>
2026-02-24 08:34:10 +0530

    ss

--- index.js
+++ index.js
@@ -46,8 +46,8 @@ mongoose.connect('mongodb://127.0.0.1:27017/joobearning')
 
 
 app.get('/data', async (req, res) => {
-    const okkk = await Promocode.find();
-    res.status(200).json(okkk)
+    
+    res.status(200).json({messsag:"done"})
 })
 
 
